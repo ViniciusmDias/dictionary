843c2fdb5e37d5fa55913ee67f21b627110dc963
Vinicius Dias <viniciusdias@pop-os.localdomain>
2022-11-29 23:24:35 -0300

    create the json web server to list all words.

--- src/components/WordList/index.tsx
+++ src/components/WordList/index.tsx
@@ -1,6 +1,24 @@
+import { useCallback, useEffect, useState } from "react";
+import { getWords } from "../../services/getWords";
 import * as S from "./styles";
 
 export function WordList() {
+  const [loading, setLoading] = useState(true);
+  const [words, setWords] = useState<string[]>([]);
+
+  const setWordListInfo = useCallback(async () => {
+    setLoading(true);
+    const newWords = await getWords(100);
+    if (newWords) {
+      setWords(newWords);
+    }
+
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setWordListInfo();
+  }, [setWordListInfo]);
   return (
     <S.WordListContainer>
       <S.WordListHeader>
@@ -11,55 +29,11 @@ export function WordList() {
       <S.WordListContent>
         <h5>Word list</h5>
         <S.List>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
-          <button>hello</button>
+          {loading ? (
+            <p>Carregando...</p>
+          ) : (
+            words.map((word) => <button key={word}>{word}</button>)
+          )}
         </S.List>
       </S.WordListContent>
     </S.WordListContainer>
